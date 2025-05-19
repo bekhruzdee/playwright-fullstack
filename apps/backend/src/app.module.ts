3a4566ab822096dfe9b85026d28623fead321d90
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-05-19 15:39:09 +0500

    update posts and users

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+@Module({
+  imports: [UsersModule, PostsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
