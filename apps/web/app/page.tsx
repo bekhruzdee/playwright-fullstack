200f099123b3307db90c13d15461601ffad4ed27
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-05-21 11:08:33 +0500

    update web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
+      <div className="text-4xl font-bold text-gray-800 mb-6 ">
+        Full Stack Playwright Demo
+      </div>
+      <nav>
+        <ul className="flex space-x-6">
+          <li>
+            <Link href="/users">
+              <span className="text-blue-600 hover:underline">Users</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/posts">
+              <span className="text-blue-600 hover:underline">Posts</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
