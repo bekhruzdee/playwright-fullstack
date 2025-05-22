ba1d8407798f696f129c6d350fbb5afd5f5a6196
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-05-22 17:02:57 +0500

    add e2e

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
-      <div className="text-4xl font-bold text-gray-800 mb-6 ">
+      <h1 className="text-4xl font-bold text-gray-800 mb-6 ">
         Full Stack Playwright Demo
-      </div>
+      </h1>
       <nav>
         <ul className="flex space-x-6">
           <li>
--- packages/e2e/tests/api/e3e.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-test.describe("API Endpoint Tests", () => {
-  test("GET /users returns an array", async ({ request }) => {
-    const response = await request.get("/users");
-    expect(response.ok()).toBeTruthy();
-    const data = await response.json();
-    expect(Array.isArray(data)).toBeTruthy();
-  });
-});
